6da081f2daae4330ffa7ab9063efc4c1ae6d4666
DhruvYadav221 <106151968+DhruvYadav221@users.noreply.github.com>
2023-03-21 18:57:25 +0530

    Create AddContact.js
    updated AddContact.js

---
+++ AddContact.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AddContact = (props) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+
+  const addContactHandler = (e) => {
+    e.preventDefault();
+    if (name === "" || email === "") {
+      alert("All fields are mandatory!");
+      return;
+    }
+    const newContact = {
+      id: Math.floor(Math.random() * 1000),
+      name: name,
+      email: email,
+    };
+    props.addContactHandler(newContact);
+    setName("");
+    setEmail("");
+    navigate("/");
+  };
+
+  return (
+    <div className="ui main">
+      <h2>Add Contact</h2>
+      <form className="ui form" onSubmit={addContactHandler}>
+        <div className="field">
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="field">
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <button className="ui button blue">Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddContact;

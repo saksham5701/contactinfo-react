2a4843076459c5767d4e8e63e4551a9aa0404675
DhruvYadav221 <106151968+DhruvYadav221@users.noreply.github.com>
2023-03-21 19:00:17 +0530

    Create App.js
    minor changes in App.js

---
+++ App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { v4 as uuidv4 } from 'uuid';
+import Header from "./Header";
+import ContactList from "./ContactList";
+import AddContact from "./AddContact";
+
+function App() {
+  const getlocalitems=()=>{
+    let list=localStorage.getItem('lists');
+
+    if(list) {
+      return JSON.parse(localStorage.getItem('lists'));
+    }
+    else {
+      return [];
+    }
+  }
+  
+  const [contacts, setContacts] = useState(getlocalitems()); 
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, {id: uuidv4(), ...contact}]); 
+  };
+  
+  const removeContactHandler = (id) => {
+    const newcontactlist = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContacts(newcontactlist);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('lists', JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div className="ui container">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/add" element={<AddContact addContactHandler={addContactHandler} />} />
+          <Route path="/" element={<ContactList contacts={contacts} getelementid={removeContactHandler} />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </div> 
+  );
+}
+
+export default App;
